fa8b0e4d38721f7dba0061699d9566dfde15f40f
ron23 <ron23@users.noreply.github.com>
2018-05-12 00:35:44 -0700

    Update index.js

--- index.js
+++ index.js
@@ -193,7 +193,7 @@ module.exports = {
             "no-unsafe-negation": "error",
             "no-unused-expressions": "off",
             "no-unused-labels": "error",
-            "no-unused-vars": "error",
+            "no-unused-vars": ["error", { varsIgnorePattern: "airbrake|should" }],
             "no-use-before-define": "off",
             "no-useless-call": "off",
             "no-useless-computed-key": "off",
